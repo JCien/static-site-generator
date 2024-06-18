9e7afe995287adca4c96662284a8660f2a63f6d9
JCien <jcienjr@gmail.com>
2024-06-18 11:36:02 -0700

    Added test for raising error

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,7 +7,7 @@ class HTMLNode:
         self.props = props
         
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("HTMLNode not implemented")
     
     def props_to_html(self):
         if self.props is None:
@@ -19,3 +19,14 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("No value provided")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_node(self):
@@ -10,6 +11,18 @@ class TestHTMLNode(unittest.TestCase):
     def test_props(self):
         node = HTMLNode("a", "value the text inside a paragraph", None, {"href": "https://www.google.com", "target": "_blank"})
         node.props_to_html()
+
+    def test_LeafNode(self):
+        node = LeafNode("a", "Hello World!", {"href": "https://www.boot.dev", "target": "_blank"})
+        print(node.to_html())
+
+    def test_LeafNode2(self):
+        node = LeafNode("p", "Hello World!", None)
+        print(node.to_html())
+    
+    def test_LeafNode_NoValue(self):
+        node = LeafNode("a", None, {"href": "https://www.boot.dev", "target": "_blank"})
+        self.assertRaises(ValueError, node.to_html)
 
 if __name__ == "__main__":
     unittest.main()
